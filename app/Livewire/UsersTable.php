8fa0d60826114fe0e3843eb7479d385a31e6fbe2
bhagatwalaparth <bhagatwalaparth0@gmail.com>
2025-08-25 22:24:01 +0530

    - Added user add functionality - Added role in user table listing

--- app/Livewire/UsersTable.php
+++ app/Livewire/UsersTable.php
@@ -6,6 +6,7 @@ namespace App\Livewire;
 use App\Models\User;
 use Rappasoft\LaravelLivewireTables\Views\Column;
 use Rappasoft\LaravelLivewireTables\DataTableComponent;
+use Illuminate\Database\Eloquent\Builder;
 
 class UsersTable extends DataTableComponent
 {
@@ -16,12 +17,21 @@ class UsersTable extends DataTableComponent
         $this->setPrimaryKey('id');
     }
 
+    public function builder(): Builder
+    {
+        return User::query()->with('roles');
+    }
+
     public function columns(): array
     {
         return [
             Column::make("ID", "id")->sortable(),
             Column::make("Name", "name")->searchable()->sortable(),
             Column::make("Email", "email")->searchable()->sortable(),
+            Column::make("Role")
+            ->label(function($row) {
+                return $row->roles->first()?->name ?? 'No Role';
+            }),
             Column::make("Created At", "created_at")->sortable(),
         ];
     }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminUserController;
 use App\Http\Controllers\CatalogController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
@@ -23,6 +24,11 @@ Route::middleware('auth')->group(function () {
     Route::get('/add-catalog', [CatalogController::class, 'create'])->name('catalog.create');
     Route::post('/add-catalog', [CatalogController::class, 'store'])->name('catalog.store');
     Route::get('/edit-catalog/{catalog}', [CatalogController::class, 'edit'])->name('catalog.edit');
+
+    // admin user routes
+    Route::get('/add-user', [UserController::class, 'create'])->name('user.create');
+    Route::post('/add-user', [UserController::class, 'store'])->name('user.store');
+    Route::get('/edit-user/{user}', [UserController::class, 'edit'])->name('user.edit');
 });
 
 require __DIR__.'/auth.php';
